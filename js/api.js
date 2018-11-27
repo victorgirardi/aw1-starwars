5a25e1e470e9e615b9d39b28f3996938f2055d02
Victor <victorh.girardi@hotmail.com>
2018-11-26 22:06:08 -0200

    Planets toggle Language

--- js/api.js
+++ js/api.js
@@ -1,6 +1,10 @@
 var wookiee = true;
 
-function toggleLanguage(){
+function startPage(){
+    wookiee = true;
+}
+
+function toggleLanguageSithJedi(){
     console.log('toggle')
     wookiee = !wookiee;
     nameSWAPI('Qui');
@@ -17,7 +21,7 @@ function toggleLanguage(){
 function nameSWAPI(id) {
     var request = new XMLHttpRequest();
     var swapi = 'https://swapi.co/api/people/';
-    thisSwapi = swapi + '?search=' + id;
+    var thisSwapi = swapi + '?search=' + id;
     if (wookiee) {
         thisSwapi += '&format=wookiee'
         request.open('GET', thisSwapi, true);
@@ -29,11 +33,43 @@ function nameSWAPI(id) {
         request.send();
 
     } else {
+        
         fetch(thisSwapi).then((obj) => {
             return obj.json();
         }).then((obj) => {
             document.getElementById(id).innerHTML = obj.results[0].name;
         })
 
+    }
+}
+//-----------------------------------------------------------------------------------------------------------------------------
+function toggleLanguagePlanets(){
+    wookiee = !wookiee;
+    planetsSWAPI('Tatooine');
+    planetsSWAPI('Hoth');
+    planetsSWAPI('Endor');
+    planetsSWAPI('Coruscant');
+    planetsSWAPI('Mustafar');
+}
+
+function planetsSWAPI(id){
+    var request = new XMLHttpRequest();
+    var swapi = 'https://swapi.co/api/planets/';
+    var thisSwapi = swapi + '?search=' + id;
+    if(wookiee){
+        thisSwapi += '&format=wookiee'
+        request.open('GET', thisSwapi, true);
+        request.onload = function () {
+            var obj = JSON.parse(request.responseText.replace(/whhuanan/g, '"whhuanan"'));
+            document.getElementById(id).innerHTML = obj.rcwochuanaoc[0].whrascwo
+            return true;
+        };
+        request.send();
+    } else {
+        fetch(thisSwapi).then((obj) => {
+            return obj.json();
+        }).then((obj) => {
+            document.getElementById(id).innerHTML = obj.results[0].name;
+        })
     }
 }
